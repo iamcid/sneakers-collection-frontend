30815ae0f3a9dd2e2dc18a6c257394e17682c9a8
iamcid <iamcid17@gmail.com>
2021-01-19 01:56:16 -0500

    creates src js files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const sneakersURL = "http://localhost:3000/sneakers"
+const commentsURL = "http://localhost:3000/comments"
+const sneakerForm = document.getElementById("sneaker-form")
+const sneakerColorway = document.getElementById("sneaker-colorway")
+const sneakerName = document.getElementById("sneaker-name")
+const sneakerBrand = document.getElementById("sneaker-brand")
+const sneakerPrice = document.getElementById("sneaker-price")
+const sneakerImage = document.getElementById("sneaker-image")
+
+
+sneakerForm.addEventListener("submit", Sneaker.submitSneaker)
+Sneaker.fetchSneakers()
